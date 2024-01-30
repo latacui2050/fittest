7a0054199003ca4a58953e40b9335d1b7706d9d9
Wenmo Cui <wenmo_cui@berkeley.edu>
2024-01-30 05:15:59 -0800

    seconddb

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.jtey2ed.mongodb.net/');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ellencui15:bXyHZOo3MPjCsoSQ@cluster0.6y62e01.mongodb.net/firstdb?retryWrites=true&w=majority/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ellencui15:bXyHZOo3MPjCsoSQ@cluster0.6y62e01.mongodb.net/seconddb?retryWrites=true&w=majority/');
 
 module.exports = mongoose.connection;
 
